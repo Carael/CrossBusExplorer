26f7af248beba2a684f58fc442fd7e9715fc03c0
Mariusz Matysek <mariusz.matysek@swisslife.ch>
2022-10-06 00:36:16 +0200

    Fixed nullable QueueProperties

--- src/Core/ServiceBus.Contracts/Types/QueueProperties.cs
+++ src/Core/ServiceBus.Contracts/Types/QueueProperties.cs
@@ -3,6 +3,6 @@ namespace CrossBusExplorer.ServiceBus.Contracts.Types;
 public record QueueProperties(    
     long MaxSizeInMegabytes,
     int MaxDeliveryCount,
-    string UserDescription,
-    string ForwardTo,
-    string ForwardDeadLetteredMessagesTo);
+    string? UserDescription,
+    string? ForwardTo,
+    string? ForwardDeadLetteredMessagesTo);
