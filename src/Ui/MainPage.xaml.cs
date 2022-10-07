9a85adf20d3814928fc1af5f8daa734ee7ad71f8
Mariusz Matysek <mariusz.matysek@swisslife.ch>
2022-10-07 18:23:29 +0200

    Initial UI blazor project

---
+++ src/Ui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Ui/MauiProgram.cs
@@ -0,0 +1,29 @@
+using CrossBusExplorer.ServiceBus;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Ui.Data;
+
+namespace Ui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+		#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		
+		builder.Services.AddSingleton<WeatherForecastService>();
+		builder.Services.AddServiceBusServices();
+
+		return builder.Build();
+	}
+}
