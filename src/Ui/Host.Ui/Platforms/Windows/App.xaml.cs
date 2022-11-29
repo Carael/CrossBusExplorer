71541e3748fb1d0245c2025f4fe930d6757ed221
Mariusz Matysek <mariuszmatysek@gmail.com>
2022-11-30 00:31:11 +0100

    Carael/windows release (#14)
    Windows x64 and arm64 release

--- src/Ui/Host.Ui/Platforms/Windows/App.xaml.cs
+++ src/Ui/Host.Ui/Platforms/Windows/App.xaml.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.UI.Xaml;
+﻿
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Maui.WinUI;
+namespace CrossBusExplorer.Host.Ui.Windows;
 
 /// <summary>
 /// Provides application-specific behavior to supplement the default Application class.
