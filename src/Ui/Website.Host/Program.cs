f969e77a4e9eda2912f5ced8eb48807c2b79f557
Mariusz Matysek <mariusz.matysek@swisslife.ch>
2022-12-07 09:19:35 +0100

    fixed ui

--- src/Ui/Website.Host/Program.cs
+++ src/Ui/Website.Host/Program.cs
@@ -21,16 +21,6 @@ builder.Services.AddBlazoredLocalStorage();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
