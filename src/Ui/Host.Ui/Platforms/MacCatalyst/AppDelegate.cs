ef73f323b0af8c30b9b82328461f1b0c104552f4
Mariusz Matysek <mariusz.matysek@swisslife.ch>
2025-11-14 08:24:11 +0100

    Removed maui and updated build agent

--- src/Ui/Host.Ui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-namespace CrossBusExplorer.Host.Ui.Platforms.MacCatalyst;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
